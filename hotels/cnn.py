e1a028124161d0490e54a4b6eeec05ece4ac2560
WillowLeaf <leo.hlf@gmail.com>
2016-05-19 17:33:23 +0800

    update.

--- hotels/cnn.py
+++ hotels/cnn.py
@@ -28,13 +28,39 @@ from legonet.models import NeuralNetwork
 from utils import Dictionary
 
 data_path = '/home/lifu/hotels/df1'
-glove200d_path = '/home/lifu/glove/glove.6B.200d.txt'
-glove100d_path = '/home/lifu/glove/glove.6B.100d.txt'
+glove_path = {200: '/home/lifu/glove/glove.6B.200d.txt', 
+              100: '/home/lifu/glove/glove.6B.100d.txt'}
 sentence_len = 256
+wv_len = 200
 
 
+def prepare_data_deprecated():
+    word_table = Dictionary(glove_path[wv_len]).to_array()[..., None]
+    
+    df = pd.read_pickle(data_path)  
+    df = df[df['vector'].notnull() & df['ratings.overall'].notnull()]
+    df = df[df['vector'].apply(len) <= sentence_len]
+    df['vector'] = df['vector'].apply(
+        lambda x: np.append(x, np.zeros(sentence_len-x.size)))
+    split = df.shape[0] // 10 * 9
+        
+    df['target'] = 1
+    df.loc[df['ratings.overall'] <= 3, 'target'] = 0
+    df.loc[df['ratings.overall'] == 5, 'target'] = 2
+    
+    X = np.array(list(df['vector']))
+    Y = df['target'].values
+    
+    X_train = X[:split]
+    Y_train = Y[:split]
+    X_dev = X[split:]
+    Y_dev = Y[split:]
+    print 'Completed data preparation!'
+    
+    return word_table, X_train, Y_train, X_dev, Y_dev
+    
 def prepare_data():
-    word_table = Dictionary(glove200d_path).to_array()[..., None]
+    word_table = Dictionary(glove_path[wv_len]).to_array()[..., None]
     
     df = pd.read_pickle(data_path)  
     df = df[df['vector'].notnull() & df['ratings.overall'].notnull()]
@@ -69,16 +95,16 @@ if __name__ == '__main__':
     model.add(Embedding([sentence_len], word_table))
 
     seq1 = Sequential('3gram')
-    seq1.add(Convolution([3, 100], 64))
-    seq1.add(Pooling([sentence_len, 100], strides=[1, 1]))
+    seq1.add(Convolution([3, wv_len], 64))
+    seq1.add(Pooling([sentence_len, wv_len], strides=[1, 1]))
     
     seq2 = Sequential('4gram')
-    seq2.add(Convolution([5, 100], 64))
-    seq2.add(Pooling([sentence_len, 100], strides=[1, 1]))
+    seq2.add(Convolution([5, wv_len], 64))
+    seq2.add(Pooling([sentence_len, wv_len], strides=[1, 1]))
         
     seq3 = Sequential('5gram')
-    seq3.add(Convolution([5, 100], 64))
-    seq3.add(Pooling([sentence_len, 100], strides=[1, 1]))
+    seq3.add(Convolution([5, wv_len], 64))
+    seq3.add(Pooling([sentence_len, wv_len], strides=[1, 1]))
     
     para = Parallel(along_dim=3)
     para.add(seq1)
--- hotels/utils.py
+++ hotels/utils.py
@@ -13,43 +13,10 @@ from nltk.tokenize import word_tokenize
 import numpy as np
 
 
-data_path = '/mnt/shared/hotels/split1'
-dest_path = '/mnt/shared/hotels/df1'
-glove100d_path = '/mnt/shared/glove/glove.6B.100d.txt'
-glove200d_path = '/mnt/shared/glove/glove.6B.200d.txt'
-
-#def embed_sentence(text, dictionary, preprocess=str.lower):
-#    text = preprocess(text)
-#    vecs = [dictionary[word] for word in word_tokenize(text) 
-#                                if word in dictionary]
-#    if vecs:
-#        return np.mean(np.array(vecs), axis=0)
-#    else:
-#        return np.zeros(next(dictionary.itervalues()).shape)
-#
-#def load_word_vectors(path):
-#    print 'Start loading word vectors...'
-#    dictionary = {}
-#    count = 0
-#    with open(path, 'r') as fp:
-#        for line in fp:
-#            count += 1
-#            segs = line.split()
-#            word = segs[0]
-#            vector = np.array(map(float, segs[1:]))
-#            dictionary[word] = vector
-#            if count % 100000 == 0:
-#                print 'Parsed %d lines.' % count
-#    print 'Completed! Established a dictionary with size: %d' % len(dictionary)
-#    return dictionary
-#    
-#def make_data_frame_deprecated(source, dest, embedding):
-#    with open(source) as fp:
-#        df = json_normalize(map(json.loads, fp))
-#    # filter non-ascii reviews
-#    df = df[df['text'].apply(lambda x: isinstance(x, str))]
-#    df['vector'] = df['text'].apply(embedding)
-#    pd.to_pickle(df, dest)
+data_path = '/home/lifu/hotels/review.txt'
+dest_path = '/home/lifu/hotels/dataframe'
+glove100d_path = '/home/lifu/glove/glove.6B.100d.txt'
+glove200d_path = '/home/lifu/glove/glove.6B.200d.txt'
 
 class Dictionary(object):
     """ # TODO: docstring
@@ -100,12 +67,14 @@ def make_data_frame(source, dest):
         tokens = filter(lambda t: len(t) > 1, tokens)
         indices = map(dictionary.index_of, tokens)
         valid_indices = filter(lambda i: i != -1, indices)
-        if len(valid_indices) < len(indices) * 0.8:
+        if (len(valid_indices) < 30) or (len(valid_indices) < len(indices) * 0.8):
             return None
         return np.array(valid_indices)
-        
-    df['vector'] = df['text'].apply(vectorizer)
-    pd.to_pickle(df, dest)
+    
+    ratings = [str(c).startswith('ratings') for c in df.columns]
+    result = df[ratings]
+    result['vector'] = df['text'].apply(vectorizer)
+    pd.to_pickle(result, dest)
     
     
 if __name__ == '__main__':
